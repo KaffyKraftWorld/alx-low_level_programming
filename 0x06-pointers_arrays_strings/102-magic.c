b1ac018d47be01b0f800c17908a3cf61335bd3b2
Kafayat Faniran <kaffykraftworld@gmail.com>
2022-09-27 08:16:03 -0700

    function that fills memory with a constant byte

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
 
-int main(void)
-{
-	int n;
-	int a[5];
-	int *p;
-
-	a[2] = 1024;
-	p = &n;
 /*
  * write your line of code here...
  * Remember:
@@ -16,8 +8,16 @@ int main(void)
  * - only one statement
  * - you are not allowed to code anything else than this line of code
  */
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
 	*(p + 5) = 98;
-/* this will make it print 98\n */
+	/* this will make it print 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
