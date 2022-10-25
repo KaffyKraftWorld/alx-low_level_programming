f902da4626d0655e32fdbfcf4d24a244ccb632ce
Kafayat Faniran <kaffykraftworld@gmail.com>
2022-10-25 05:59:14 +0000

    a function that returns the sum of all the data (n) of a listint_t linked list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,35 +1,22 @@
 #include "lists.h"
+#include <stdlib.h>
+
 /**
- * sum_listint - sum all the data (n) of listint_t list
- * @head: a pointer to the head of the list
- * Return: 0 if the list is empty or the sum of all the data
+ * sum_listint - sums a listint list
+ *
+ * @head: head of list
+ *
+ * Return: sum of list
  */
 int sum_listint(listint_t *head)
 {
-	int sum = 0;
+	int sum;
 
-	while (head)
-	{
-		sumofnodes(head, &sum);
-		return (sum);
-	}
-	return (0);
-}
-/**
- * sumofnodes - find the sum of ndoes
- * @head: a pointer to the head of the lintint_t list
- * @sum: integer that sums
- * Description: function to recursively find the sum of nodes
- * of the given linked list
- * Return: the sum
- */
-void sumofnodes(listint_t *head, int *sum)
-{
-	int n;
+	if (head == NULL)
+		return (0);
 
-	if (!head)
-	return;
-	n = head->n;
-	sumofnodes(head->next, sum);
-	*sum = *sum + n;
+	for (sum = 0; head != NULL; head = head->next)
+		sum += head->n;
+
+	return (sum);
 }
