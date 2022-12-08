b471386f72308973c51465a514395331ab1cf344
Kafayat Faniran <kaffykraftworld@gmail.com>
2022-12-08 22:17:06 +0000

    doubled linked list task

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - show us the nth node of a DLL
+ * @head: a pointer to the head of the DLL
+ * @index: index of the node to return its value (main = index 5 = 98)
+ * Return: value of the node in the index or NULL if doesn't exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *current_index;
+	unsigned int idx_runner;
+
+	if (head == NULL)
+		return (NULL);
+
+	current_index = head;
+	head->n = current_index->n;
+	idx_runner = 0;
+
+	while (current_index != NULL)
+	{
+		if (idx_runner == index)
+			return (current_index);
+		idx_runner++;
+		current_index = current_index->next;
+	}
+	return (NULL);
+}
